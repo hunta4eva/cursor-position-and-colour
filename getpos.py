49853014b726c683733e0acd0231073b709841c6
hunta4eva <68666048+hunta4eva@users.noreply.github.com>
2020-08-08 23:37:06 +0400

    Update getpos.py

--- getpos.py
+++ getpos.py
@@ -1,11 +1,11 @@
 import pyautogui
 while True:
     x, y = pyautogui.position()
-    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-    pixelColor = pyautogui.screenshot().getpixel((x, y))
-    positionStr += '      RGB: (' + str(pixelColor[0]).rjust(3)
-    positionStr += ', ' + str(pixelColor[1]).rjust(3)
-    positionStr += ', ' + str(pixelColor[2]).rjust(3) + ')'
-    print(positionStr, end='')
+    string_position = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    pixelcolor = pyautogui.screenshot().getpixel((x, y))
+    string_position += '      RGB: (' + str(pixelcolor[0]).rjust(3)
+    string_position += ', ' + str(pixelcolor[1]).rjust(3)
+    string_position += ', ' + str(pixelcolor[2]).rjust(3) + ')'
+    print(string_position, end='')
     print('\b' * len(positionStr), end='', flush=True)
 
